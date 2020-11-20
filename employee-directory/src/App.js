798cc37d67fd8d54193999d902e1555aca37350c
Zinc <greenberg.zachary@utexas.edu>
2020-11-19 19:51:56 -0600

    almost working

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,25 +1,157 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import SearchInput from "./components/SearchInput";
+import TableHeader from "./components/TableHeader";
+import SearchResults from "./components/SearchResults";
+import employees from './employee.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    employees: employees,
+    search: "",
+    sortOrder: "ascending" 
+  }
+
+  handleInputChange = event => {
+    let query = event.target.value.toLowerCase();
+    const filteredEmployees = employees.filter(result =>
+      result.name.toLowerCase().toString().includes(query) ||
+      result.birthday.toLowerCase().toString().includes(query) ||
+      result.email.toLowerCase().toString().includes(query) ||
+      result.phone.toLowerCase().toString().includes(query) 
+    );
+
+    this.setState({ employees: filteredEmployees })
+  }
+
+  handleFormSubmit = () => {
+    event.preventDefault();
+  }
+
+  // get sort order here
+  compareSortASC(a, b) {
+    if (a.name > b.name) return 1;
+    if (b.name > a.name) return -1;
+    return 0;
+  };
+  compareSortDESC(a, b) {
+    if (a.name > b.name) return -1;
+    if (b.name > a.name) return 1;
+    return 0;
+  };
+  handleSortClick = () => {
+    event.preventDefault();
+    const sortOrderNow = this.state.sortOrder == "descending" ? "ascending" : "descending";
+    this.setState({ sortOrder: sortOrderNow })
+    let sortedEmployees = "";
+    if (this.state.sortOrder === "ascending") {
+      sortedEmployees = this.state.employees.sort(this.compareSortASC);
+    } else if (this.state.sortOrder === "descending") {
+      sortedEmployees = this.state.employees.sort(this.compareSortDESC);
+    }
+    this.setState({ employees: sortedEmployees })
+  }
+
+  compareSortASCbday(a, b) {
+    if (Date.parse(a.birthday) > Date.parse(b.birthday)) return 1;
+    if (Date.parse(b.birthday) > Date.parse(a.birthday)) return -1;
+    return 0;
+  };
+  compareSortDESCbday(a, b) {
+    if (Date.parse(a.birthday) > Date.parse(b.birthday)) return -1;
+    if (Date.parse(b.birthday) > Date.parse(a.birthday)) return 1;
+    return 0;
+  };
+  handleSortClickBday = () => {
+    event.preventDefault();
+    const sortOrderNow = this.state.sortOrder == "descending" ? "ascending" : "descending";
+    this.setState({ sortOrder: sortOrderNow })
+    let sortedEmployees = "";
+    if (this.state.sortOrder === "ascending") {
+      sortedEmployees = this.state.employees.sort(this.compareSortASCbday);
+    } else if (this.state.sortOrder === "descending") {
+      sortedEmployees = this.state.employees.sort(this.compareSortDESCbday);
+    }
+    this.setState({ employees: sortedEmployees })
+  }
+
+  compareSortASCphone(a, b) {
+    if (a.phone > b.phone) return 1;
+    if (b.phone > a.phone) return -1;
+    return 0;
+  };
+  compareSortDESCphone(a, b) {
+    if (a.phone > b.phone) return -1;
+    if (b.phone > a.phone) return 1;
+    return 0;
+  };
+  handleSortClickPhone = () => {
+    event.preventDefault();
+    const sortOrderNow = this.state.sortOrder == "descending" ? "ascending" : "descending";
+    this.setState({ sortOrder: sortOrderNow })
+    let sortedEmployees = "";
+    if (this.state.sortOrder === "ascending") {
+      sortedEmployees = this.state.employees.sort(this.compareSortASCphone);
+    } else if (this.state.sortOrder === "descending") {
+      sortedEmployees = this.state.employees.sort(this.compareSortDESCphone);
+    }
+    this.setState({ employees: sortedEmployees })
+  }
+
+  compareSortASCemail(a, b) {
+    if (a.email > b.email) return 1;
+    if (b.email > a.email) return -1;
+    return 0;
+  };
+  compareSortDESCemail(a, b) {
+    if (a.email > b.email) return -1;
+    if (b.email > a.email) return 1;
+    return 0;
+  };
+  handleSortClickEmail = () => {
+    event.preventDefault();
+    const sortOrderNow = this.state.sortOrder == "descending" ? "ascending" : "descending";
+    this.setState({ sortOrder: sortOrderNow })
+    let sortedEmployees = "";
+    if (this.state.sortOrder === "ascending") {
+      sortedEmployees = this.state.employees.sort(this.compareSortASCemail);
+    } else if (this.state.sortOrder === "descending") {
+      sortedEmployees = this.state.employees.sort(this.compareSortDESCemail);
+    }
+    this.setState({ employees: sortedEmployees })
+  }
+
+  render() {
+    return (
+      <>
+        <Header />
+        <SearchInput
+          name="search" value={this.state.search}
+          handleFormSubmit={this.handleFormSubmit}
+          handleInputChange={this.handleInputChange}
+        />
+        <table className="table table-striped">
+          <TableHeader 
+          handleSortClick={this.handleSortClick}
+          handleSortClickBday={this.handleSortClickBday}
+          handleSortClickPhone={this.handleSortClickPhone}
+          handleSortClickEmail={this.handleSortClickEmail} />
+          <tbody>
+          {this.state.employees.map(individual =>
+            <SearchResults
+              name={individual.name}
+              image={individual.image}
+              phone={individual.phone}
+              email={individual.email}
+              birthday={individual.birthday}
+              id={individual.id}
+              key={individual.id}
+            />)}
+        </tbody>
+        </table>
+      </>
+    );
+  }
 }
 
 export default App;
